0eb30decf4fcd26031674f6c7ea84c127bc67aab
bcskda <demulkup@ya.ru>
2021-01-12 15:38:38 +0300

    Main: clean exit on signals

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python
 import os
+from signal import signal, SIGINT, SIGTERM
 from tornado.ioloop import IOLoop
 from guardian import Guardian
 from bot import NiceNotifyBot
 from listener import make_app, DEFAULT_PORT
+
+STOP_SIGNALS = [SIGINT, SIGTERM]
+
+def prepare_signals():
+    def stop_ioloop(signo):
+        IOLoop.current().stop()
+        print(f'Stopped IOLoop on signal {signo}')
+
+    def stop_on_signal(signo, frame):
+        print(f'Stopping IOLoop on signal {signo}')
+        # Sync-edly request IOLoop stop
+        IOLoop.current().add_callback_from_signal(stop_ioloop, signo)
+
+    for signo in STOP_SIGNALS:
+        signal(signo, stop_on_signal)
 
 def main():
     auth_path = os.environ['BALTHIFY_AUTH_PATH']
@@ -12,8 +28,12 @@ def main():
     guard = Guardian()
     port = int(os.environ.get('BALTHIFY_LISTEN_PORT') or DEFAULT_PORT)
     app = make_app(auth_path, bot, guard)
+
+    prepare_signals()
     app.listen(port)
-    IOLoop.current().start()
+    bot.updater.start_polling()
+    IOLoop.current().start() # Will be stopped by signal handler
+    bot.updater.stop()
 
 if __name__ == '__main__':
     main()
