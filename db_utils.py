d42bfec16ec10cadb16cdc583d5ed8737cded59b
bcskda <demulkup@ya.ru>
2021-01-14 15:02:29 +0300

    SafeSessionFactory

--- db_utils.py
+++ db_utils.py
@@ -1,61 +1,32 @@
+from concurrent.futures import ThreadPoolExecutor
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from db_models import metadata
-from sync_utils import BaseSafeSession, AwaitableSafeSession, FutureSafeSession
+from sync_utils import BaseSafeWrapper, AwaitableSafeWrapper, FutureSafeWrapper
 from logs import get_logger
 
 logger = get_logger('db_utils')
 
-engine = None
-session_factory = None
+class SafeSessionFactory:
+    SAFE_CLASSES = {
+        'future': FutureSafeWrapper,
+        'asyncio': AwaitableSafeWrapper
+    }
 
-def init_engine(uri):
-    '''Throws ValueError if called twice.'''
-    if engine is not None:
-        raise ValueError('engine already initialized')
-    logger.info('initializing engine')
-    engine = create_engine(uri, encoding='utf-8')
-    metadata.create_all(engine, checkfirst=True)
-    session_factory = sessionmaker(engine)
-    logger.info('initialized engine')
+    def __init__(self, uri):
+        self.engine = create_engine(uri, encoding='utf-8')
+        metadata.create_all(self.engine, checkfirst=True)
+        self.factory = sessionmaker(bind=self.engine)
+        self.executor = ThreadPoolExecutor(max_workers=1)
+        self.session = self.executor.submit(self.factory).result()
 
-session = None
-session_executor = ThreadPoolExecutor(max_workers=1)
+    def __call__(self, kind: str):
+        return self.SAFE_CLASSES[kind](self.session, self.executor)
 
-SAFE_CLASSES = {
-    'future': FutureSafeSession,
-    'awaitable': AwaitableSafeSession
-}
-
-def _init_session():
-    if session is not None:
-        raise ValueError('session already initialized')
-
-    def _do_init_session():
-        session = session_factory()
-        logger.info('initialized session')
-
-    logger.info('initializing session')
-    session_executor.submit(_do_init_session).result()
-
-def get_session(kind: str):
-    '''Get a safe-wrapper for session.
-
-    Arguments:
-    kind: 'future' or 'awaitable'
-    '''
-    if session is None:
-        _init_session()
-    return safety_classes[kind](session, session_executor)
-
-def shutdown():
-    '''Properly shutdown serial executor and close session'''
-    def _do_close_sessions():
-        session_factory.close_all()
-        logger.info('closed all sessions')
-
-    logger.info('closing all sessions')
-    logger.info('stopping session executor')
-    session_executor.submit(_do_close_sessions)
-    session_executor.shutdown()
-    logger.info('stopped session executor')
+    def shutdown(self):
+        logger.info('%s: shutting down sessions', self)
+        self.executor.submit(self.session.close).result()
+        logger.info('%s: shut down sessions', self)
+        logger.info('%s: shutting down executor', self)
+        self.executor.shutdown()
+        logger.info('%s: shut down executor', self)
