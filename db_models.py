417891afee570ad1d9cd2fbe76ff8b8075504b38
bcskda <demulkup@ya.ru>
2021-01-16 18:23:03 +0300

    db_models: add fields

--- db_models.py
+++ db_models.py
@@ -1,14 +1,19 @@
-from sqlalchemy import Column, MetaData
+from sqlalchemy import Column, ForeignKey, MetaData, Sequence
 from sqlalchemy import DateTime, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 metadata = MetaData()
 Base = declarative_base(metadata=metadata)
 
-class ScheduledStream(Base):
-    __tablename__ = 'ScheduledStream'
+class RoutingRecord(Base):
+    __tablename__ = 'RoutingRecord'
+    routing_id = Column(Integer, Sequence('routing_id_seq'),
+                        primary_key=True)
     ingress_app = Column(String, nullable=False)
-    ingress_id = Column(String(32), primary_key=True)
+    ingress_id = Column(String(32), nullable=False)
     egress_app = Column(String, nullable=False)
     egress_id = Column(String(32), nullable=False)
     start_time = Column(DateTime, nullable=False)
+    end_time = Column(DateTime, nullable=False)
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=False)
