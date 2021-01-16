23f9e0967d5ab73ff0d13deb918d7522ee388eaf
bcskda <demulkup@ya.ru>
2021-01-16 19:38:52 +0300

    config: more params

--- config.py
+++ config.py
@@ -11,13 +11,20 @@ class Config:
     _get_or_none = lambda key: os.environ.get('BALTHIFY_' + key)
 
     auth_path = _get('AUTH_PATH')
+    listen_port = int(_get_or_none('LISTEN_PORT') or DEFAULT_LISTEN_PORT)
+    
     chat_id = _get('CHAT_ID')
     admin_id = _get('ADMIN_ID')
     tg_token = _get('TOKEN')
+    
     db_uri = _get('DB_URI')
-    listen_port = int(_get_or_none('LISTEN_PORT') or DEFAULT_LISTEN_PORT)
+    
     ingress_app = _get('APP_INGRESS')
     egress_app = _get('APP_EGRESS')
     rtmp_port = int(_get_or_none('RTMP_PORT') or DEFAULT_RTMP_PORT)
+    rtmp_addr = _get('RTMP_ADDR')
+    
     redirect_template = 'rtmp://127.0.0.1:{}/{{}}/{{}}'.format(rtmp_port)
+    access_template = 'rtmp://{}:{}/{{}}/{{}}'.format(rtmp_addr, rtmp_port)
+    
     loglevel = getattr(logging, _get_or_none('LOGLEVEL') or 'DEBUG')
