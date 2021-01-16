b8efcab47f2c4e1cb0a0c875da340fa7789f4213
bcskda <demulkup@ya.ru>
2021-01-16 20:00:03 +0300

    loglevel in config

--- balthify/logs.py
+++ balthify/logs.py
@@ -1,6 +1,7 @@
 import logging
+from balthify.config import Config
 
 def get_logger(name):
     logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(Config.loglevel)
     return logger
